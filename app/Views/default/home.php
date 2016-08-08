43a2e17cf6ff60d63230726198c5ea15a6cf1090
ttissi <ttissi00@gmail.com>
2016-08-08 12:56:09 +0200

    MAJ Caroussel
    caroussel.php + plugins.js

--- app/Views/default/home.php
+++ app/Views/default/home.php
@@ -1,7 +1,5 @@
 <?php $this->layout('layout', ['title' => 'Accueil']) ?>
 
 <?php $this->start('main_content') ?>
-
-	<h2>Chargement OK => GO pour le code !!</h2>
 	
 <?php $this->stop('main_content') ?>
--- public/assets/js/plugins.js
+++ public/assets/js/plugins.js
@@ -22,3 +22,16 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+// Caroussel
+$(function () {
+    $('#homeCarousel').carousel({
+        interval:3000,
+        pause: "false"
+    });
+    $('#playButton').click(function () {
+        $('#homeCarousel').carousel('cycle');
+    });
+    $('#pauseButton').click(function () {
+        $('#homeCarousel').carousel('pause');
+    });
+});
